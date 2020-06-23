459959eee904239dd4af9ea678df50097820a0a0
sam03-git <62543915+sam03-git@users.noreply.github.com>
2020-06-23 12:32:40 +0900

    add files

---
+++ app.js
@@ -0,0 +1,105 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+var scores, roundScore, activePlayer;
+
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+/*dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);*/
+
+//document.querySelector('#current-' + activePlayer).textContent = dice; //select one
+//Math.random() * 6
+//Math.floor() -> gets rid od decimals
+
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+//Events: Notifications that are sent to notify the code that something happened on the webpage
+// Ex) clicking buttons, resizing windows, scrolling down, or pressing a key
+
+//Eent listener: A function that performs an action based on a certain event. 
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+    // 3. Update the round score IF the rolled number was Not a 1
+    if (dice > 1){ //!==
+        // Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+        nextPlayer();
+    }
+    
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add current scor eto GLOBAL score
+    scores[activePlayer] += roundScore;
+    
+    // Update the UI 
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    // Check if player won the game
+    if (scores[activePlayer] >= 100){
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        }else{
+        nextPlayer();
+            
+        }
+    
+});
+
+function nextPlayer () {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+        
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+        
+        
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+    document.querySelector('.dice').style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
