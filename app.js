42b2c1ed1b9fb30b5aa849ed32f2bd6bc7044aca
samComeIt <62543915+sam03-git@users.noreply.github.com>
2020-06-23 16:26:16 +0900

    added function for New Game button

--- app.js
+++ app.js
@@ -9,12 +9,7 @@ GAME RULES:
 
 */
 var scores, roundScore, activePlayer;
-
-
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
+init();
 /*dice = Math.floor(Math.random() * 6) + 1;
 console.log(dice);*/
 
@@ -94,6 +89,30 @@ function nextPlayer () {
         //document.querySelector('.player-1-panel').classList.add('active');
     document.querySelector('.dice').style.display = 'none';
 }
+
+document.querySelector('.btn-new').addEventListener('click', init);
+    
+function init(){
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    
+    
+   
+    
+};
 
 
 
